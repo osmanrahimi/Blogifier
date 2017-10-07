6f7558e3f2cf5ac7bb0c48cab560f36046d33ba0
rxtur <rtur.net@gmail.com>
2017-10-07 11:44:16 -0500

    Admin settings/users page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Blogifier.Core.Common;
+using Blogifier.Core.Data.Domain;
+using Blogifier.Core.Data.Interfaces;
+using Blogifier.Core.Data.Models;
+using Blogifier.Core.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogifier.Controllers
+{
+    [Authorize]
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        IUnitOfWork _db;
+        string _theme;
+
+        public AdminController(IUnitOfWork db)
+        {
+            _db = db;
+            _theme = "~/Views/Blogifier/Admin/" + ApplicationSettings.AdminTheme + "/";
+        }
+
+        [VerifyProfile]
+        [Route("settings/users")]
+        public IActionResult Import()
+        {
+            return View(_theme + "Settings/Users.cshtml", new AdminBaseModel { Profile = GetProfile() });
+        }
+
+        private Profile GetProfile()
+        {
+            return _db.Profiles.Single(b => b.IdentityName == User.Identity.Name);
+        }
+    }
+}
