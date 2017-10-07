b93b44b2fd57a38437b5b95403d0dcfa76e4cf91
Francis <francisdotbio@gmail.com>
2017-10-07 21:11:41 +0330

    users

---
+++ wwwroot/blogifier/admin/onethree/js/app/usersController.js
@@ -0,0 +1,15 @@
+﻿var itemCheck = $('.item-checkbox');
+var firstItemCheck = itemCheck.first();
+
+// check all
+$(firstItemCheck).on('change', function () {
+    $(itemCheck).prop('checked', this.checked);
+    toggleActionBtns();
+});
+
+// uncheck "check all" when one item is unchecked
+$(itemCheck).not(firstItemCheck).on('change', function () {
+    if ($(this).not(':checked')) {
+        $(firstItemCheck).prop('checked', false);
+    }
+});
